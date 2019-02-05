97479413ef9b59c7ee4fa066c7b62b2739a42c79
arxaqapi <arxaqapi@gmail.com>
2019-02-05 22:16:26 +0100

    cleanup

--- index.js
+++ index.js
@@ -113,23 +113,7 @@ app.post('/login', (req, res) => {
 
 })
 app.use(function(req, res, next) {
-
-  res.writeHead(404);
-  //res.render("404.ejs", {reqUrl: req.url})
-
-
-  res.status(404).send('Page introuvable !');
-
-})
-
-
-app.use(function(req, res, next) {
-
-  //res.render("404.ejs", {reqUrl: req.url})
-
-
   res.status(404).render("404.ejs", {reqUrl: req.url});
-
 })
 
 app.listen(8080, () => {
