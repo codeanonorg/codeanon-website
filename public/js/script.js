3088560eab3d0266609997be7ea3f34bda494fc5
arxaqapi <arxaqapi@protonmail.com>
2019-08-03 18:05:44 +0200

    useless js file import removed

--- public/js/script.js
+++ public/js/script.js
@@ -1,33 +1,3 @@
-function getData() {
-    let pass = document.getElementById('pass').value
-    let name = document.getElementById('email').value
-    return {
-        email: name,
-        password: CryptoJS.SHA256(pass).toString()
-    }
-};
-
-function login() {
-    let data = getData()
-    return fetch('/', {
-        method: "POST",
-        mode: "cors",
-        cache: "no-cache",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        redirect: "follow",
-        body: JSON.stringify(data)
-    })
-};
-
-
-function submit() {
-    login()
-    return false
-};
-
-
 function toggle(elementId) {
 	let element = document.getElementById(elementId);
 	if(element.style.display == "block") {
