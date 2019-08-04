d3f26fd2a558dd7f8c2601e61c035bc0e339a829
arxaqapi <arxaqapi@protonmail.com>
2019-08-04 16:28:59 +0200

    replaced msg with username

--- routes/home.js
+++ routes/home.js
@@ -4,7 +4,7 @@ const router = require('express').Router()
 router.get('/', (req, res) => {
     if (req.session.user) {
         res.render('home.ejs', {
-            msg: req.session.user.username
+            username: req.session.user.username
         })
     } else {
         res.redirect('/')
