32f72e39eb88e1f9cab686eec8983e99995aa12c
arxaqapi <arxaqapi@protonmail.com>
2019-08-14 23:42:40 +0200

    new home page

--- routes/home.js
+++ routes/home.js
@@ -4,7 +4,8 @@ const router = require('express').Router()
 router.get('/', (req, res) => {
     if (req.session.user) {
         res.render('home.ejs', {
-            username: req.session.user.username
+            username: req.session.user.username,
+            page: "Home",
         })
     } else {
         res.redirect('/')
