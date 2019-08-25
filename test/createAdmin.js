9de23c62acec9675eb483b456306f12db3231576
arxaqapi <arxaqapi@protonmail.com>
2019-08-25 22:42:59 +0200

    cretae admin updated

--- test/createAdmin.js
+++ test/createAdmin.js
@@ -1,12 +1,38 @@
-const mongo = require('mongodb').MongoClient
-const databaseUrl = 'mongodb://localhost:27017'//db' //27017 default port
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt')
 const saltRounds = 10;
 
-mongo.connect(databaseUrl, {useNewUrlParser: true}, (err, client) => {
-    const db = client.db('CodeAnonDatabase')
-    const userCol = db.collection('users')
-    userCol.insertOne({username: "admin", email: "admin@admin.admin", hashedPassword: bcrypt.hashSync("admin", saltRounds)})
-    client.close()
-    console.log("admin created");
-  })
+const { Pool, Client } = require('pg')
+
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'catest',
+    password: '',
+    port: 5432,
+})
+
+pool.query('SELECT NOW()', (err, res) => {
+    console.log(err, res)
+    pool.end()
+})
+
+const client = new Client({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'catest',
+    password: '',
+    port: 5432,
+})
+
+client.connect()
+
+const user_sql = 'INSERT INTO users(username, real_name, email, password, timestamp, role_id, status_id) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *' 
+const pass = bcrypt.hashSync("admin", saltRounds)
+const value_user = ['admin', 'realAdmin', 'admin@codeanon.org', pass,'123455', '3', '1']
+
+client
+    .query(user_sql, value_user)
+    .then(res => {
+        console.log(res.rows[0])
+    })
+    .catch(e => console.error(e.stack))
