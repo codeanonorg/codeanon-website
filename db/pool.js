6dde319f4598ca9085e1a72b68cd7c792cab245b
Arthur Correnson <arthur.correnson@gmail.com>
2019-09-02 14:38:50 +0200

    glue heroku code

--- db/pool.js
+++ db/pool.js
@@ -1,11 +1,8 @@
 const { Pool } = require('pg')
 
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'catest',
-    password: 'dev',
-    port: 5432,
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
 })
 
 
