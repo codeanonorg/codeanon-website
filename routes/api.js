c47d9db0c6b558dfbc8ec1e977fb82f423487d24
arxaqapi <arxaqapi@protonmail.com>
2019-11-16 15:09:12 +0100

    api object fix

--- routes/api.js
+++ routes/api.js
@@ -17,7 +17,8 @@ apiRoute.get('/articles/top3', function (req, res) {
     articleQuery.getTenMostRecentArticles()
     .then(queryResponse => {
         let dateArray = time.getDatesForArticleList(queryResponse.rows)
-        res.status(200).send(queryResponse.rows, dateArray)
+
+        res.status(200).send(Object.assign({}, queryResponse.rows, dateArray))
         })
         .catch(e => {
             console.error(e.stack)
