445e233ba252dd754f2ec8ee070c01b467af2526
arxaqapi <arxaqapi@protonmail.com>
2019-11-16 15:15:37 +0100

    awful json test

--- routes/api.js
+++ routes/api.js
@@ -18,7 +18,8 @@ apiRoute.get('/articles/top3', function (req, res) {
     .then(queryResponse => {
         let dateArray = time.getDatesForArticleList(queryResponse.rows)
 
-        res.status(200).send(Object.assign({}, queryResponse.rows, dateArray))
+        ///res.status(200).send(Object.assign({}, queryResponse.rows, dateArray))
+        res.status(200).json(queryResponse.rows)
         })
         .catch(e => {
             console.error(e.stack)
